18022bfc5167e3d4e69eec8e69a427dbf9024709
mizu-hisa <albicocca.m.1031@gmail.com>
2022-07-01 14:51:01 +0000

    復習：投稿機能準備まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  
+
   root to: "homes#top"
-  
-  get 'about' => 'homes#about', as: 'about'
+
+  get '/homes/about' => 'homes#about', as: 'about'
+
+  resources :post_images, only: [:new, :index, :show]
+  # resourcesで一括まとめる
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
 end
