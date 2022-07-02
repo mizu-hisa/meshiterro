862825b10c91b3248f257dbfc796942ed865f6f5
mizu-hisa <albicocca.m.1031@gmail.com>
2022-07-02 14:14:08 +0000

    復習：ユーザ側機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   get '/homes/about' => 'homes#about', as: 'about'
+  
+  resources :users, only: [:show, :edit]
+  # resourcesで一括まとめる
+  # get 'users/show'
+  # get 'users/edit'
 
   resources :post_images, only: [:new, :index, :create, :show, :destroy]
   # resourcesで一括まとめる
