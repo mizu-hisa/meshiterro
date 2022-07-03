ed2c3a07f76ee5dc491ad572b7cf036b50b52e7f
mizu-hisa <albicocca.m.1031@gmail.com>
2022-07-03 14:41:01 +0000

    復習：コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,19 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   get '/homes/about' => 'homes#about', as: 'about'
-  
+
   resources :users, only: [:show, :edit, :update]
   # resourcesで一括まとめる
   # get 'users/show'
   # get 'users/edit'
 
-  resources :post_images, only: [:new, :index, :create, :show, :destroy]
+  resources :post_images, only: [:new, :index, :create, :show, :destroy] do
   # resourcesで一括まとめる
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+
 end
